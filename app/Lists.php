1671f1be399df7b60c41f4f3169ad2203709e80c
Sai Neeraj Kanuri <neerajkanuri@MacBook-Pro-10.local>
2018-07-05 17:49:25 +0530

    First commit

---
+++ app/Lists.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lists extends Model
+{
+    //
+    protected $fillable = ['name','user_id'];
+
+    public function users()
+    {
+        return $this->belongsTo(Users::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Tasks::class);
+    }
+}
---
+++ app/Tasks.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tasks extends Model
+{
+    //
+    protected $fillable = ['description','list_id'];
+
+    public function lists()
+    {
+        return $this->belongsTo(Lists::class);
+    }
+}
