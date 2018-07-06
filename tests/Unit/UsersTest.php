ff736c3c6643605abc35fac35a79b91a632131a4
Sai Neeraj Kanuri <neerajkanuri@MacBook-Pro-10.local>
2018-07-06 16:15:43 +0530

    add user working - one test added

--- tests/Unit/UsersTest.php
+++ tests/Unit/UsersTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Users;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -16,5 +17,14 @@ class UsersTest extends TestCase
     public function testExample()
     {
         $this->assertTrue(true);
+    }
+
+    public function testAddUser()
+    {
+        $users = new Users();
+        $result = response()->json($users->addUser('neeraj','abcd'));
+        $this->assertSame(json_encode([
+            'status' => 'User exists'
+        ]), $result->getContent());
     }
 }
